6576ea2ea193df55096fb3788a1e10539c9ad3e6
Jeisson Hidalgo <jeissonh@gmail.com>
2014-05-23 11:41:53 -0600

    Reingenería básica. Modificados nombres de variables y métodos. Documentación en Doxygen. Cambios de indentación.

--- Classes/ChainReactionScene.cpp
+++ Classes/ChainReactionScene.cpp
@@ -23,75 +23,39 @@ Scene* ChainReactionScene::createScene()
 // on "init" you need to initialize your instance
 bool ChainReactionScene::init()
 {
-	//////////////////////////////
-	// 1. super init first
-	if ( !Layer::init() )
-	{
-		return false;
-	}
+	// Init parent first
+	if ( !Layer::init() ) return false;
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Point origin = Director::getInstance()->getVisibleOrigin();
 
-	/////////////////////////////
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-
-	// add a "close" icon to exit the progress. it's an autorelease object
-	auto closeItem = MenuItemImage::create(
-										   "CloseNormal.png",
-										   "CloseSelected.png",
-										   CC_CALLBACK_1(ChainReactionScene::menuCloseCallback, this));
-
-	closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-								origin.y + closeItem->getContentSize().height/2));
-
-	// create menu, it's an autorelease object
+	// Add a menu item with "X" image, which is clicked to quit the program
+	auto closeItem = MenuItemImage::create("CloseNormal.png", "CloseSelected.png", CC_CALLBACK_1(ChainReactionScene::menuCloseCallback, this));
+	closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2, origin.y + closeItem->getContentSize().height/2));
 	auto menu = Menu::create(closeItem, NULL);
 	menu->setPosition(Point::ZERO);
 	this->addChild(menu, 1);
 
-
-
-	/////////////////////////////
-	// 3. add your codes below...
-
-	// add a label shows "Hello World"
-	// create and initialize a label
-
+	// Create and place the original balls on the scenery
 	loadBalls();
 	for ( size_t i = 0; i < 13; ++i )
 		addRandomBall();
 
-	remainingTimeLabel = ColorfulLabel::create("CatchMe", "Fonts/NextGames.ttf", 24);
+	// Create and place the label to show the remaining time
+	_remainingTimeLabel = ColorfulLabel::create("CatchMe", "Fonts/NextGames.ttf", 24);
+	_remainingTimeLabel->setPosition(Point(origin.x + visibleSize.width/2, origin.y + visibleSize.height - _remainingTimeLabel->getContentSize().height));
+	this->addChild(_remainingTimeLabel, 1);
 
-	// position the label on the center of the screen
-	remainingTimeLabel->setPosition(Point(origin.x + visibleSize.width/2,
-							origin.y + visibleSize.height - remainingTimeLabel->getContentSize().height));
+	// Create and place the label to show the score of current player
+	_currenPlayerScore = Scores::create("0000", "Fonts/NextGames.ttf", 24);
+	_currenPlayerScore->setPosition(Point(origin.x + visibleSize.width - _currenPlayerScore->getContentSize().width, origin.y + visibleSize.height - _currenPlayerScore->getContentSize().height));
+	addChild(_currenPlayerScore, 1);
 
-	// add the label as a child to this layer
-	this->addChild(remainingTimeLabel, 1);
-
-	scores = Scores::create("0000", "Fonts/NextGames.ttf", 24);
-	scores->setPosition(Point(origin.x + visibleSize.width - scores->getContentSize().width,
-							origin.y + visibleSize.height - scores->getContentSize().height));
-	addChild(scores, 1);
-
-#if 0
-	// add "ChainReactionScene" splash screen"
-	auto sprite = Sprite::create("ChainReactionScene.png");
-
-	// position the sprite on the center of the screen
-	sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-	// add the sprite as a child to this layer
-	this->addChild(sprite, 0);
-#endif
-
+	// Call update methods the
 	schedule( schedule_selector(ChainReactionScene::update) );
-	schedule( schedule_selector(ChainReactionScene::updateTimer), 1.0f);
-	setEventHandlers();
+	schedule( schedule_selector(ChainReactionScene::updateRemainingTime), 1.0f);
 
+	setEventHandlers();
 	return true;
 }
 
@@ -111,12 +75,12 @@ void ChainReactionScene::setEventHandlers()
 
 void ChainReactionScene::update(float df)
 {
-	for ( size_t i = 0; i < _ballClones.size(); ++i)
+	for ( size_t i = 0; i < _ballClones.size(); ++i )
 	{
 		_ballClones[i]->update(df);
-		if ( explosion && Util::collides(explosion, _ballClones[i] ) )
+		if ( _lastExplosion && Util::collides(_lastExplosion, _ballClones[i] ) )
 		{
-			++explosionCount;
+			++_chainedExplosionCount;
 			ParticleSystemQuad* particleSystem = ParticleExplosion::create();
 			this->addChild(particleSystem, 10);
 			particleSystem->setTexture( Director::getInstance()->getTextureCache()->addImage("Stars2.png") );
@@ -125,22 +89,22 @@ void ChainReactionScene::update(float df)
 			particleSystem->setDuration(1.0f);
 
 			// Update the score
-			scores->addPoints(explosionCount);
+			_currenPlayerScore->addPoints(_chainedExplosionCount);
 
-			log("Removing child %zi" , i + 1);
+			log("Removing child %zi", i + 1);
 			removeChild(_ballClones[i]);
 			_ballClones.erase( _ballClones.begin() + i );
 		}
 	}
 }
 
-void ChainReactionScene::updateTimer(float df)
+void ChainReactionScene::updateRemainingTime(float df)
 {
-	remainingTime -= 1.0f;
+	_remainingTime -= 1.0f;
 	char buffer[32];
-	sprintf(buffer, "%.0f", remainingTime );
+	sprintf(buffer, "%.0f", _remainingTime );
 
-	remainingTimeLabel->setString(buffer);
+	_remainingTimeLabel->setString(buffer);
 }
 
 void ChainReactionScene::loadBalls()
@@ -162,11 +126,11 @@ void ChainReactionScene::addRandomBall()
 
 bool ChainReactionScene::placeExplosion(Touch* touch, Event* event)
 {
-	if ( explosion ) return false;
+	if ( _lastExplosion ) return false;
 
-	explosion = Sprite::create("Explosion.png");
-	explosion->setPosition( touch->getLocation() );
-	addChild(explosion, 2);
+	_lastExplosion = Sprite::create("Explosion.png");
+	_lastExplosion->setPosition( touch->getLocation() );
+	addChild(_lastExplosion, 2);
 	return true;
 }
 
--- Classes/ChainReactionScene.h
+++ Classes/ChainReactionScene.h
@@ -12,41 +12,48 @@ class Scores;
 class ChainReactionScene : public cocos2d::Layer
 {
   protected:
-	/// Los 7 frames para crear bolas
+	/// Original images used to create balls on the scenery
+	/// These are just image files
 	std::vector<cocos2d::SpriteFrame*> _ballFrames;
-
-	/// Las n bolas en la pantalla que ve el jugador
+	/// The n balls created on the screen using the ball frames as basis
 	std::vector<Ball*> _ballClones;
-
-	cocos2d::Sprite* explosion = nullptr;
-
-	ColorfulLabel* remainingTimeLabel = nullptr;
-	float remainingTime = 60.0f;
-
-	Scores* scores = nullptr;
-	size_t explosionCount = 0;
+	/// Pointer to the last explosion created on the scenery, nullptr if there is no
+	/// currently an explosion.
+	cocos2d::Sprite* _lastExplosion = nullptr;
+	/// Player has initially a minute to create as many explosions as s/he can
+	float _remainingTime = 60.0f;
+	/// A label to show the remaining time to the user
+	ColorfulLabel* _remainingTimeLabel = nullptr;
+	/// A label showing the score of the current player on the screen
+	Scores* _currenPlayerScore = nullptr;
+	/// Score for an explosion increses when there is a chain. Each explosion has its own value.
+	/// The first explosion credits for 1 point, the second explosion in the same chain credits
+	/// for 2 points, and so on. Therefore a counter is required to count the number of explosions
+	/// in the current chain
+	size_t _chainedExplosionCount = 0;
 
   public:
-	// there's no 'id' in cpp, so we recommend returning the class instance pointer
+	/// there's no 'id' in cpp, so we recommend returning the class instance pointer
 	static cocos2d::Scene* createScene();
-
-	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	/// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
 	virtual bool init();
-
-	// a selector callback
+	/// a selector callback
 	void menuCloseCallback(cocos2d::Ref* pSender);
-
-	// implement the "static create()" method manually
+	/// implement the "static create()" method manually
 	CREATE_FUNC(ChainReactionScene)
-
 	/// Se invoca 60 veces por segundo, cada vez que se actualiza la pantalla
 	void update(float df);
-	void updateTimer(float df);
+	/// Called once each second in order to update the time ramining label
+	void updateRemainingTime(float df);
 
   protected:
+	/// Create the original set of balls when the game is loaded
 	void loadBalls();
+	/// .
 	void setEventHandlers();
+	/// Creates a ball from the frames array and places it on some place of the screen
 	void addRandomBall();
+	/// .
 	bool placeExplosion(cocos2d::Touch* touch, cocos2d::Event *event);
 };
 
